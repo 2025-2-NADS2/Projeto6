2cd4e49ea4c00bdb32492f10db67d8288d0199fc
Rogério Rodrigues Vieira <101807154+Roferoro@users.noreply.github.com>
2025-09-22 12:45:50 -0300

    Colocando POO e ED na pasta src

---
+++ src/Entrega 1/Estrutura de Dados/EntregaEstruturaDados/EntregaEstruturaDados/tipoAtv.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntregaEstruturaDados
+{
+    internal class tipoAtv
+    {
+        public int id { get; set; }
+        public String tipo { get; set; }
+
+        public tipoAtv(int id, string tipo)
+        {
+            this.id = id;
+            this.tipo = tipo;
+        }
+
+        public bool Maior(tipoAtv atv)
+        {
+            return this.id > atv.id;
+
+        }
+
+
+    }
+}
